a2f710e81c25d5afe668d31b1cdc766d93ec0914
lndsilva <lndsilva@hotmail.com>
2021-06-09 20:19:39 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/apprecyclerview2h_12021/FilmesAdapter.java
@@ -0,0 +1,51 @@
+package br.com.local.apprecyclerview2h_12021;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class FilmesAdapter extends RecyclerView.Adapter<FilmesAdapter.ViewHolder>{
+
+    Context ctx;
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+
+
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+
+        holder.txtNomeCard.setText("Outro Cartão");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView txtNomeCard;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtNomeCard = itemView.findViewById(R.id.txtNome);
+
+        }
+    }
+}
---
+++ app/src/main/java/br/com/local/apprecyclerview2h_12021/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.local.apprecyclerview2h_12021;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.idRecyclerView);
+
+
+        //recyclerView.setAdapter(adaptador);
+    }
+}
